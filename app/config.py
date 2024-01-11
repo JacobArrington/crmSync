777c3aca5ce593bb79541113e1fb90296cbc6023
Jacob Arrington <Jacobsarrington@gmail.com>
2024-01-10 19:29:44 -0500

    fixed import error

--- app/config.py
+++ app/config.py
@@ -4,17 +4,10 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLAlchemy 1.4 no longer supports url strings that start with 'postgres'
+    # (only 'postgresql') but heroku's postgres add-on automatically sets the
+    # url in the hidden config vars to start with postgres.
+    # so the connection uri must be updated here (for production)
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL').replace('postgres://', 'postgresql://')
     SQLALCHEMY_ECHO = True
-
-    # Get the DATABASE_URL from the environment variables
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-
-    # Check if DATABASE_URL is set and modify it if necessary
-    if DATABASE_URL:
-        # SQLAlchemy 1.4+ no longer supports url strings that start with 'postgres://'
-        # (only 'postgresql://') but Heroku's Postgres add-on sets the URL starting with 'postgres://'.
-        # Hence, the connection URI must be updated here for production.
-        SQLALCHEMY_DATABASE_URI = DATABASE_URL.replace('postgres://', 'postgresql://')
-    else:
-        # Handle the case where DATABASE_URL is not set
-        SQLALCHEMY_DATABASE_URI = None
