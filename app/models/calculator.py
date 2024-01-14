11cbb539eb7bd512216900cfd6015c3553ae52f8
Jacob Arrington <Jacobsarrington@gmail.com>
2024-01-14 15:58:50 -0500

    added calc and widget relationships

--- app/models/calculator.py
+++ app/models/calculator.py
@@ -1,20 +1,19 @@
 from .db import db,environment,SCHEMA, add_prefix_for_prod
 
-class Calculator(db.model):
+class Calculator(db.Model):
     __tablename__ = 'calculators'
 
     if environment == "production":
         __table_args__={'schema': SCHEMA}
 
         id = db.Column(db.Integer, primary_key=True)
-        widgetId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('widgets.id')))
+        widgetId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('widgets.id')), default=1)
         balance = db.Column(db.Numeric(precision=10, scale=2))
         lump_sum = db.Column(db.Numeric(precision=10, scale=2))
         remainder_after_lump = db.Column(db.Numeric(precision=10, scale=2))
         interest_rate = db.Column(db.Numeric(precision=4, scale=2))
         installment_options = db.Column(db.Integer)
         custom_installment = db.Column(db.Integer)
-       
         installment_amount = db.Column(db.Numeric(precision=10, scale=2))
         discount = db.Column(db.Numeric(precision=3, scale=2))
         custom_discount = db.Column(db.Numeric(precision=3, scale=2))
@@ -22,5 +21,10 @@ class Calculator(db.model):
         custom_monthly_payment =db.Column(db.Numeric(precision=10, scale=2))
         custom_months_to_pay =db.Column(db.Integer)
 
-        
-        
+
+        widget = db.relationship('Widget',
+                                primaryjoin='Widget.id ==Calculator.widgetId',
+                                back_populates='calculator',
+                                useList=False,
+                                viewonly=True,
+                                )
--- app/models/widget.py
+++ app/models/widget.py
@@ -1,4 +1,9 @@
 from .db import db,environment,SCHEMA, add_prefix_for_prod
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base=declarative_base()
 
 class Widget(db.Model):
     __tablename__ ="widgets"
@@ -11,10 +16,14 @@ class Widget(db.Model):
         user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')))
         widget_type = db.Column(db.String(255))
 
+         # one to many
         user = db.relationship('User', back_populates= 'widgets' )
 
+        #polymorphic 
+        calculator = db.relationship('Calculator', userList=False, back_populates = 'widget')
 
-        def widget_to_dict(self):
+
+        def to_widget_dict(self):
             return{
                 'id': self.id,
                 'userId': self.user_id,
