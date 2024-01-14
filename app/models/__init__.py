b8971175a0ffc24746a34bfa936f93844b237631
Jacob Arrington <Jacobsarrington@gmail.com>
2024-01-14 18:16:43 -0500

    added imports to init.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,9 @@
 from .db import db
 from .user import User
+from .widget import Widget
+from .calculator import Calculator
+from .scratch_pad import ScratchPad
+from .sticky_note import StickyNote
+from .goal import Goal
+from .hot_list import HotList
 from .db import environment, SCHEMA
