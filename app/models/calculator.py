9d4302017495d562c72eba1f2ca8722909d398ce
Jacob Arrington <Jacobsarrington@gmail.com>
2024-01-14 15:12:57 -0500

    calc model made next relationship with widget

---
+++ app/models/calculator.py
@@ -0,0 +1,26 @@
+from .db import db,environment,SCHEMA, add_prefix_for_prod
+
+class Calculator(db.model):
+    __tablename__ = 'calculators'
+
+    if environment == "production":
+        __table_args__={'schema': SCHEMA}
+
+        id = db.Column(db.Integer, primary_key=True)
+        widgetId = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod('widgets.id')))
+        balance = db.Column(db.Numeric(precision=10, scale=2))
+        lump_sum = db.Column(db.Numeric(precision=10, scale=2))
+        remainder_after_lump = db.Column(db.Numeric(precision=10, scale=2))
+        interest_rate = db.Column(db.Numeric(precision=4, scale=2))
+        installment_options = db.Column(db.Integer)
+        custom_installment = db.Column(db.Integer)
+       
+        installment_amount = db.Column(db.Numeric(precision=10, scale=2))
+        discount = db.Column(db.Numeric(precision=3, scale=2))
+        custom_discount = db.Column(db.Numeric(precision=3, scale=2))
+        total_after_discount =db.Column(db.Numeric(precision=10, scale=2))
+        custom_monthly_payment =db.Column(db.Numeric(precision=10, scale=2))
+        custom_months_to_pay =db.Column(db.Integer)
+
+        
+        
